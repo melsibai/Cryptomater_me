92833dd7868dc059131b01ca4b08b2dcc164da6c
melsibai <mostafa.sibai@gmail.com>
2026-02-16 11:44:37 +0100

    TRY_3

--- domain/src/main/java/org/cryptomator/domain/usecases/DoLicenseCheck.java
+++ domain/src/main/java/org/cryptomator/domain/usecases/DoLicenseCheck.java
@@ -44,24 +44,24 @@ public class DoLicenseCheck {
 	}
 
 	 public LicenseCheck execute() throws BackendException {
--               license = useLicenseOrRetrieveFromDb(license);
--               try {
--                       Algorithm algorithm = Algorithm.ECDSA512(getPublicKey(ANDROID_PUB_KEY), null);
--                       JWTVerifier verifier = JWT.require(algorithm).build();
--                       DecodedJWT jwt = verifier.verify(license);
--                       return jwt::getSubject;
--               } catch (SignatureVerificationException | JWTDecodeException | FatalBackendException e) {
--                       if (e instanceof SignatureVerificationException && isDesktopSupporterCertificate(license)) {
--                               throw new DesktopSupporterCertificateException(license);
-+               return new LicenseCheck() {
-+                       public String mail() {
-+                               return "";
+              license = useLicenseOrRetrieveFromDb(license);
+             try {
+                       Algorithm algorithm = Algorithm.ECDSA512(getPublicKey(ANDROID_PUB_KEY), null);
+                       JWTVerifier verifier = JWT.require(algorithm).build();
+                       DecodedJWT jwt = verifier.verify(license);
+                      return jwt::getSubject;
+               } catch (SignatureVerificationException | JWTDecodeException | FatalBackendException e) {
+                       if (e instanceof SignatureVerificationException && isDesktopSupporterCertificate(license)) {
+                               throw new DesktopSupporterCertificateException(license);
+               return new LicenseCheck() {
+                       public String mail() {
+                               return "";
                         }
--                       throw new LicenseNotValidException(license);
--               } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
--                       throw new FatalBackendException(e);
--               }
-+               };
+                       throw new LicenseNotValidException(license);
+               } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+                       throw new FatalBackendException(e);
+               }
+               };
         }
  
 
