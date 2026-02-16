812ddff5c7062ab5e898c1fb797f73a3a1e0de78
melsibai <mostafa.sibai@gmail.com>
2026-02-16 12:03:47 +0100

    Refactor execute method in DoLicenseCheck_5
    Refactor execute method to return LicenseCheck instance with JWT subject.

--- domain/src/main/java/org/cryptomator/domain/usecases/DoLicenseCheck.java
+++ domain/src/main/java/org/cryptomator/domain/usecases/DoLicenseCheck.java
@@ -43,26 +43,33 @@ public class DoLicenseCheck {
 		this.license = license;
 	}
 
-	 public LicenseCheck execute() throws BackendException {
-              license = useLicenseOrRetrieveFromDb(license);
-             try {
-                       Algorithm algorithm = Algorithm.ECDSA512(getPublicKey(ANDROID_PUB_KEY), null);
-                       JWTVerifier verifier = JWT.require(algorithm).build();
-                       DecodedJWT jwt = verifier.verify(license);
-                      return jwt::getSubject;
-               } catch (SignatureVerificationException | JWTDecodeException | FatalBackendException e) {
-                       if (e instanceof SignatureVerificationException && isDesktopSupporterCertificate(license)) {
-                               throw new DesktopSupporterCertificateException(license);
-               return new LicenseCheck() {
-                       public String mail() {
-                               return "";
-                        }
-                       throw new LicenseNotValidException(license);
-               } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
-                       throw new FatalBackendException(e);
-               }
-            
+	public LicenseCheck execute() throws BackendException {
+    license = useLicenseOrRetrieveFromDb(license);
+
+    try {
+        Algorithm algorithm = Algorithm.ECDSA512(getPublicKey(ANDROID_PUB_KEY), null);
+        JWTVerifier verifier = JWT.require(algorithm).build();
+        DecodedJWT jwt = verifier.verify(license);
+
+        // Return a LicenseCheck instance
+        return new LicenseCheck() {
+            @Override
+            public String mail() {
+                return jwt.getSubject(); // return the subject from JWT
+            }
+        };
+
+    } catch (SignatureVerificationException | JWTDecodeException | FatalBackendException e) {
+        if (e instanceof SignatureVerificationException && isDesktopSupporterCertificate(license)) {
+            throw new DesktopSupporterCertificateException(license);
+        } else {
+            throw new LicenseNotValidException(license);
         }
+    } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+        throw new FatalBackendException(e);
+    }
+}
+
  
 
 	private String useLicenseOrRetrieveFromDb(String license) throws NoLicenseAvailableException {
